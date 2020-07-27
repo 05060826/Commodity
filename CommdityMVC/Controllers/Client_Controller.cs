8f444a8087f29df8ed95a2555a43dc818a08b867
lei zhao <wwwzhao12@qq.com>
2020-07-27 20:29:31 +0800

    006

--- CommdityMVC/Controllers/Client_Controller.cs
+++ CommdityMVC/Controllers/Client_Controller.cs
@@ -16,12 +16,14 @@ namespace Commodity.Controllers
         {
             return View();
         }
-        public IActionResult ADD()
+        //结算
+        public IActionResult ADD(string rid)
         {
+            ViewBag.rid = rid;
             return View();
         }
-        //结算
-        public IActionResult Account(int rid)
+        //结算2
+        public IActionResult Account(string rid)
         {
             ViewBag.rid = rid;
             return View();
