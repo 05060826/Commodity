18b6bf5621934a3f7e1250c7cd77dcf370460def
zjt301 <2647259942@qq.com>
2020-07-27 17:28:54 +0800

    ..

---
+++ DataAccess/Model/ReplyModel.cs
@@ -0,0 +1,13 @@
+﻿using DataAccess.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Model
+{
+    public class ReplyModel
+    {
+        public string Isbn { get; set; }
+        public List<string> ReplyA { get; set; }
+    }
+}
